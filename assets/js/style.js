b1ebf9e8badf1faeb45a76b9f7b57bb5959c21c2
paramveersoni1 <paramveer.tr.mdb@gmail.com>
2022-01-20 13:00:39 +0530

    my changes

--- assets/js/style.js
+++ assets/js/style.js
@@ -8,4 +8,53 @@
     });
  });
 
- 
+ var owl = $('.owl-carousel');
+ owl.owlCarousel({
+  loop:true,
+  nav:true,
+  margin:10,
+  autoplayTimeout: 2000,
+  autoplay: true,
+
+  responsive: {
+    0: {
+      items: 3
+    },
+    600: {
+      items: 6
+    },
+    500: {
+      items: 6
+    }
+  }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+  if (e.deltaY>0) {
+      owl.trigger('next.owl');
+  } else {
+      owl.trigger('prev.owl');
+  }
+  e.preventDefault();
+});
+
+function onClose() {
+  document.getElementById('croses').style.display = "none";
+}
+
+let first = false;
+
+function toggle() {
+  if (!first) {
+    first = true;
+    document.getElementById('fdsd').style.display = "block";
+  } else {
+    first = false;
+    document.getElementById('fdsd').style.display = "none";
+  }
+}
+
+AOS.init({
+  offset: 100,
+  duration: 500,
+  easing: 'liner',
+});
