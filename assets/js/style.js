4d268675a7c184dd975928f011403ec79088a724
paramveersoni1 <paramveer.tr.mdb@gmail.com>
2022-01-21 16:24:48 +0530

    kdjf

--- assets/js/style.js
+++ assets/js/style.js
@@ -117,16 +117,13 @@ jQuery(function($){
 	$("#desktop-nav").menuScroll(topMenuHeight);
 });
 
-// COPY THE FOLLOWING FUNCTION INTO ANY SCRIPTS
 jQuery.fn.extend({
     menuScroll: function(offset) {
-		// Declare all global variables
         var topMenu = this;
 		var topOffset = offset ? offset : 0;
         var menuItems = $(topMenu).find("a");
         var lastId;
 	
-		// Save all menu items into scrollItems array
         var scrollItems = $(menuItems).map(function() {
             var item = $($(this).attr("href"));
             if (item.length) {
@@ -134,7 +131,6 @@ jQuery.fn.extend({
             }
         });
 
-		// When the menu item is clicked, get the #id from the href value, then scroll to the #id element
         $(topMenu).on("click", "a", function(e){
             var href = $(this).attr("href");
             
@@ -147,25 +143,21 @@ jQuery.fn.extend({
 
         });
 		
-		// When page is scrolled
         $(window).scroll(function(){
             var nm = $("html").scrollTop();
             var nw = $("body").scrollTop();
             var fromTop = (nm > nw ? nm : nw)+topOffset;
 
 			
-			// When the page pass one #id section, return all passed sections to scrollItems and save them into new array current
             var current = $(scrollItems).map(function(){
                 if ($(this).offset().top <= fromTop)
                 return this;
             });
 			
-			// Get the most recent passed section from current array
             current = current[current.length-1];
             var id = current && current.length ? current[0].id : "";
             if (lastId !== id) {
                 lastId = id;
-                // Set/remove active class
                 $(menuItems)
                 .parent().removeClass("active")
                 .end().filter("[href='#"+id+"']").parent().addClass("active");
