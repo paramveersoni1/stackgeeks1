4848f6613b2b1b067e90b57839133a1a20fd88e7
paramveersoni1 <paramveer.tr.mdb@gmail.com>
2022-01-24 14:41:50 +0530

    sdd

--- assets/js/style.js
+++ assets/js/style.js
@@ -173,13 +173,13 @@ const swiper = new Swiper('.swiper-container', {
 		delay: 900,
  	},
 	
-	coverflowEffect: {
-		rotate: 50,
-		stretch: 0,
-		depth: 200,
-		modifier: 1,
-		slideShadows: false,
-	},
+	// coverflowEffect: {
+	// 	rotate: 50,
+	// 	stretch: 0,
+	// 	depth: 200,
+	// 	modifier: 1,
+	// 	slideShadows: false,
+	// },
 	
 	breakpoints: {
 		320: {
