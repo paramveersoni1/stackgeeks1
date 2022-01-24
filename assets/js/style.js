b120b5194e48f7cbd35edc4f846578e65b538592
paramveersoni1 <paramveer.tr.mdb@gmail.com>
2022-01-24 12:54:58 +0530

    dsd

--- assets/js/style.js
+++ assets/js/style.js
@@ -161,6 +161,49 @@ jQuery.fn.extend({
     }
 });
 
+const swiper = new Swiper('.swiper-container', {
+	effect: 'coverflow',
+	centeredSlides: true,
+  clickable: true,
+	slidesPerView: 1,
+	loop: true,
+	speed: 900,
+	
+ 	autoplay: {
+		delay: 900,
+ 	},
+	
+	coverflowEffect: {
+		rotate: 50,
+		stretch: 0,
+		depth: 200,
+		modifier: 1,
+		slideShadows: false,
+	},
+	
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		560: {
+			slidesPerView: 3,
+		},
+		990: {
+			slidesPerView: 4,
+		},
+	},
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+
 
 
 
